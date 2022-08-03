4fe692db7fb67ac118ac897cde3e97bf56ac3d99
Boyce007 <diboyce21@students.desu.edu>
2022-08-03 16:53:52 -0400

    Done!

--- src/main/java/com/codedifferently/Anagram01.java
+++ src/main/java/com/codedifferently/Anagram01.java
@@ -1,9 +1,42 @@
 package com.codedifferently;
 
 
+import java.util.*;
+
 public class Anagram01 {
 
     public static String[] problem(String inputWord, String[] inputWordList){
-        return null;
+        // create hasmap
+        // collect everyword along with letter count
+        // loop thorught inputwordlist
+        Map<Character,Integer> map = countLetters(inputWord);
+        List<String> result = new ArrayList<>();
+        int y = 0;
+        for (int i = 0; i < inputWordList.length; i++) {
+          Map<Character,Integer> currentWord = countLetters(inputWordList[i]);
+          for (Character value: currentWord.keySet()) {
+                if(!map.containsKey(value)) {
+                    continue;
+              }
+              result.add(inputWordList[i]);
+           }
+        }
+
+
+        return result.toArray(new String[0]);
+    }
+
+
+    public static Map<Character,Integer> countLetters(String inputWord) {
+        Map<Character,Integer> map = new HashMap<>();
+        for (int i = 0; i < inputWord.length(); i++) {
+            if(map.containsKey(inputWord.charAt(i))) {
+                Integer value = map.get(inputWord.charAt(i)) + 1;
+                map.put(inputWord.charAt(i),value);
+            } else{
+                map.put(inputWord.charAt(i),1);
+            }
+        }
+        return map;
     }
 }
